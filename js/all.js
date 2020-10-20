87797111b784b7706af9ec8b4a58f6ee8f5226ac
LaHabita <desde.la.habita@gmail.com>
2020-10-21 01:43:18 +0200

    nav

--- js/all.js
+++ js/all.js
@@ -2,3 +2,17 @@ $( window ).on( "load", function() {
 	 $(".load").hide();
 	 $('body').fadeIn();
 });
+
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
