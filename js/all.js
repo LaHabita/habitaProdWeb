3f03eb689adfb35920e7cbe8880f49b5262dfa3a
LaHabita <desde.la.habita@gmail.com>
2020-10-26 21:28:38 +0100

    imgs

--- js/all.js
+++ js/all.js
@@ -1,18 +1,4 @@
-$( window ).on( "load", function() {
-	 $(".load").hide();
+$( document ).ready(function() {
+	$(".load").hide();
 	 $('body').fadeIn();
 });
-
-
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
-
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
